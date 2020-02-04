9b053621a9904b1250ef7f5d624f9439cb588dbf
rrep <pwduerr@gmail.com>
2020-02-04 10:43:31 -0700

    delete todo

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -2,7 +2,9 @@
     <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
         <p>
             <input type="checkbox" v-on:change="markComplete">
-            {{todo.title}}</p>
+            {{todo.title}}
+            <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+            </p>
     </div>
 </template>
 
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
     <div v-bind:key="todo.id" v-for="todo in todos">
-        <todo-item v-bind:todo="todo"/>
+        <todo-item v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)"/>
     </div>
     </div>    
 </template>
